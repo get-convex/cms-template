9b3fcd1ffba227fe9468ec22fecb7afc4f718030
Ian Macartney <ianhmacartney@gmail.com>
2024-10-10 14:13:14 -0700

    prettier

--- convex/posts.ts
+++ convex/posts.ts
@@ -4,9 +4,10 @@ import schema, { posts } from "./schema";
 import { crud } from "convex-helpers/server/crud";
 import { Triggers } from "convex-helpers/server/triggers";
 import { DataModel, type Doc } from "./_generated/dataModel";
-import { customMutation, customCtx } from "convex-helpers/server/customFunctions";
-
-
+import {
+  customMutation,
+  customCtx,
+} from "convex-helpers/server/customFunctions";
 
 // Use Triggers to keep aggregated 'search' field up-to-date
 // by automatically updating it whenever a post is created/updated.
@@ -14,245 +15,256 @@ import { customMutation, customCtx } from "convex-helpers/server/customFunctions
 const triggers = new Triggers<DataModel>();
 
 triggers.register("posts", async (ctx, change) => {
-    console.log('Triggered on posts with change: ', change)
-    if (change.newDoc) { // A post has been inserted or updated;
-        // aggregate the relevant searchable text fields
-        // into a single 'search' string to be indexed
-        const { title, content, summary, slug } = change.newDoc
-        const newSearch = [title, content, summary, slug].join(' ');
+  console.log("Triggered on posts with change: ", change);
+  if (change.newDoc) {
+    // A post has been inserted or updated;
+    // aggregate the relevant searchable text fields
+    // into a single 'search' string to be indexed
+    const { title, content, summary, slug } = change.newDoc;
+    const newSearch = [title, content, summary, slug].join(" ");
 
-        // if the search aggregate has changed, update it
-        if (change.newDoc.search !== newSearch) {
-            console.log(`Triggering search field update for ${slug} (post ${change.id})`)
-            await ctx.db.patch(change.id, { search: newSearch });
-        }
+    // if the search aggregate has changed, update it
+    if (change.newDoc.search !== newSearch) {
+      console.log(
+        `Triggering search field update for ${slug} (post ${change.id})`,
+      );
+      await ctx.db.patch(change.id, { search: newSearch });
     }
+  }
 });
 // Wrap the default Convex mutation function to be aware of our trigger
 const mutation = customMutation(rawMutation, customCtx(triggers.wrapDB));
 
-
 // Use the trigger-wrapped mutation function to generate CRUD helpers.
 // See also: https://stack.convex.dev/crud-and-rest
-export const {
-    create,
-    read,
-    update,
-    destroy
-} = crud(schema, 'posts', query, mutation);
-
-
+export const { create, read, update, destroy } = crud(
+  schema,
+  "posts",
+  query,
+  mutation,
+);
 
 export const publish = mutation({
-    args: {
-        versionId: v.id('versions')
-    },
-    handler: async (ctx, args) => {
-        const version = await ctx.db.get(args.versionId)
-        if (!version) {
-            throw new Error(`Version ${args.versionId} not found`);
-        }
-        const { _id, _creationTime, editorId: _editorId,
-            postId, ...content } = version;
-        const oldPost = await ctx.db.get(postId);
-
-        if (!oldPost) {
-            throw new Error(`Post ${version.postId} not found`);
-        }
-        const patch = {
-            ...content,
-            published: true,
-            publishTime: oldPost.publishTime || Date.now(),
-            updateTime: Date.now(),
-        }
-        await ctx.db.patch(postId, patch);
-        return read(ctx, { id: postId });
+  args: {
+    versionId: v.id("versions"),
+  },
+  handler: async (ctx, args) => {
+    const version = await ctx.db.get(args.versionId);
+    if (!version) {
+      throw new Error(`Version ${args.versionId} not found`);
     }
-})
+    const {
+      _id,
+      _creationTime,
+      editorId: _editorId,
+      postId,
+      ...content
+    } = version;
+    const oldPost = await ctx.db.get(postId);
 
+    if (!oldPost) {
+      throw new Error(`Post ${version.postId} not found`);
+    }
+    const patch = {
+      ...content,
+      published: true,
+      publishTime: oldPost.publishTime || Date.now(),
+      updateTime: Date.now(),
+    };
+    await ctx.db.patch(postId, patch);
+    return read(ctx, { id: postId });
+  },
+});
 
 export const list = query({
-    args: {},
-    handler: async (ctx) => {
-        // If the user is authenticated, include unpublished drafts
-        // by omitting the index filter. Otherwise, use the index
-        // filter to only return published posts
-        const authed = await ctx.auth.getUserIdentity()
+  args: {},
+  handler: async (ctx) => {
+    // If the user is authenticated, include unpublished drafts
+    // by omitting the index filter. Otherwise, use the index
+    // filter to only return published posts
+    const authed = await ctx.auth.getUserIdentity();
 
-        const posts = await ctx.db.query("posts")
-            .withIndex('by_published',
-                authed ? undefined : q => q.eq('published', true)
-            )
-            .order("desc")
-            .collect();
+    const posts = await ctx.db
+      .query("posts")
+      .withIndex(
+        "by_published",
+        authed ? undefined : (q) => q.eq("published", true),
+      )
+      .order("desc")
+      .collect();
 
-
-        return Promise.all(
-            posts.map(async (post) => {
-                // Add the author's details to each post.
-                const author = (await ctx.db.get(post.authorId))!;
-                return { ...post, author };
-            }),
-        );
-    },
+    return Promise.all(
+      posts.map(async (post) => {
+        // Add the author's details to each post.
+        const author = (await ctx.db.get(post.authorId))!;
+        return { ...post, author };
+      }),
+    );
+  },
 });
 
 export const getById = query({
-    args: {
-        id: v.id('posts'),
-        withAuthor: v.optional(v.boolean())
-    },
-    handler: async (ctx, args) => {
-        const { id, withAuthor } = args
-        const post = await ctx.db.get(id);
-        if (!post) return null;
-        let author;
-        if (withAuthor) {
-            // Add the author's details to each post.
-            author = (await ctx.db.get(post.authorId))!;
-        }
-        return { ...post, author };
-
+  args: {
+    id: v.id("posts"),
+    withAuthor: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const { id, withAuthor } = args;
+    const post = await ctx.db.get(id);
+    if (!post) return null;
+    let author;
+    if (withAuthor) {
+      // Add the author's details to each post.
+      author = (await ctx.db.get(post.authorId))!;
     }
-})
-
-type PostAugmented = Doc<'posts'> & {
-    publicVersion?: Doc<'versions'>;
-    draft?: Doc<'versions'>;
-    author?: Doc<'users'>;
-};
-export const getBySlug = query({
-    args: {
-        slug: v.string(),
-        withDraft: v.optional(v.boolean()),
-        withAuthor: v.optional(v.boolean())
-    },
-    handler: async (ctx, args) => {
-        const versionsBySlug = ctx.db.query('versions')
-            .withIndex('by_slug', q => q.eq('slug', args.slug))
-
-        const publicVersion = await versionsBySlug
-            .filter(q => q.eq(q.field('published'), true))
-            .order('desc')
-            .first();
-
-        let post = await ctx.db.query('posts')
-            .withIndex('by_slug', q => q.eq('slug', args.slug))
-            .order('desc')
-            .first();
-
-        if (!post) {
-            // The slug for this post may have changed
-            // try searching for this slug in old versions
-            if (publicVersion) {
-                // The slug is outdated, lookup the postId
-                post = await ctx.db.get(publicVersion.postId);
-            }
-        }
-        if (!post) return null; // The slug is unknown
-
-        const authed = await ctx.auth.getUserIdentity()
-        if (!authed && !post.published) {
-            // This is an unpublished draft, unauthenticated
-            // user does not have permission to view it
-            return null;
-        }
-
-        const result: PostAugmented = post;
-        if (publicVersion) {
-            result.publicVersion = publicVersion;
-        }
-
-        if (args.withDraft) {
-            // Find the most recent unpublished draft, if any,
-            // created after this post was last updated
-            const draft = await ctx.db.query('versions')
-                .withIndex('by_postId', q => q.eq('postId', post._id))
-                .filter(q => q.and(
-                    q.gt(q.field('_creationTime'),
-                        post.updateTime || post._creationTime),
-                    q.eq(q.field('published'), false)
-                ))
-                .order('desc')
-                .first();
-            if (draft) {
-                result.draft = draft;
-            }
-        }
-
-        if (args.withAuthor) {
-            const author = await ctx.db.get(post.authorId);
-            if (author) {
-                result.author = author
-            }
-        }
-
-        return result;
-    }
+    return { ...post, author };
+  },
 });
 
+type PostAugmented = Doc<"posts"> & {
+  publicVersion?: Doc<"versions">;
+  draft?: Doc<"versions">;
+  author?: Doc<"users">;
+};
+export const getBySlug = query({
+  args: {
+    slug: v.string(),
+    withDraft: v.optional(v.boolean()),
+    withAuthor: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const versionsBySlug = ctx.db
+      .query("versions")
+      .withIndex("by_slug", (q) => q.eq("slug", args.slug));
+
+    const publicVersion = await versionsBySlug
+      .filter((q) => q.eq(q.field("published"), true))
+      .order("desc")
+      .first();
+
+    let post = await ctx.db
+      .query("posts")
+      .withIndex("by_slug", (q) => q.eq("slug", args.slug))
+      .order("desc")
+      .first();
+
+    if (!post) {
+      // The slug for this post may have changed
+      // try searching for this slug in old versions
+      if (publicVersion) {
+        // The slug is outdated, lookup the postId
+        post = await ctx.db.get(publicVersion.postId);
+      }
+    }
+    if (!post) return null; // The slug is unknown
+
+    const authed = await ctx.auth.getUserIdentity();
+    if (!authed && !post.published) {
+      // This is an unpublished draft, unauthenticated
+      // user does not have permission to view it
+      return null;
+    }
+
+    const result: PostAugmented = post;
+    if (publicVersion) {
+      result.publicVersion = publicVersion;
+    }
+
+    if (args.withDraft) {
+      // Find the most recent unpublished draft, if any,
+      // created after this post was last updated
+      const draft = await ctx.db
+        .query("versions")
+        .withIndex("by_postId", (q) => q.eq("postId", post._id))
+        .filter((q) =>
+          q.and(
+            q.gt(
+              q.field("_creationTime"),
+              post.updateTime || post._creationTime,
+            ),
+            q.eq(q.field("published"), false),
+          ),
+        )
+        .order("desc")
+        .first();
+      if (draft) {
+        result.draft = draft;
+      }
+    }
+
+    if (args.withAuthor) {
+      const author = await ctx.db.get(post.authorId);
+      if (author) {
+        result.author = author;
+      }
+    }
+
+    return result;
+  },
+});
 
 export const isSlugTaken = query({
-    args: {
-        slug: v.string(),
-        postId: posts.withoutSystemFields.postId
-    },
-    handler: async (ctx, args) => {
-        const { slug, postId } = args;
+  args: {
+    slug: v.string(),
+    postId: posts.withoutSystemFields.postId,
+  },
+  handler: async (ctx, args) => {
+    const { slug, postId } = args;
 
-        // Find any existing post(s) with this slug and flag
-        // any whose postId doesn't match the one given
-        const posts = await ctx.db.query('posts')
-            .withIndex('by_slug', q => q.eq('slug', slug))
-            .collect();
-        const badPostIds = new Set(
-            posts.filter(p => p._id !== postId).map(p => p._id)
-        );
+    // Find any existing post(s) with this slug and flag
+    // any whose postId doesn't match the one given
+    const posts = await ctx.db
+      .query("posts")
+      .withIndex("by_slug", (q) => q.eq("slug", slug))
+      .collect();
+    const badPostIds = new Set(
+      posts.filter((p) => p._id !== postId).map((p) => p._id),
+    );
 
-        // It's possible that the slug is no longer in use on any posts,
-        // but had previously been used in an old version of another post.
-        // Collect all version(s) with this slug...
-        const versions = await ctx.db.query('versions')
-            .withIndex('by_slug', q => q.eq('slug', slug))
-            .collect();
-        // ...and flag any whose postId doesn't match the one given.
-        const badVersions = versions.filter(v => v.postId !== postId);
-        badVersions.map(v => badPostIds.add(v.postId))
+    // It's possible that the slug is no longer in use on any posts,
+    // but had previously been used in an old version of another post.
+    // Collect all version(s) with this slug...
+    const versions = await ctx.db
+      .query("versions")
+      .withIndex("by_slug", (q) => q.eq("slug", slug))
+      .collect();
+    // ...and flag any whose postId doesn't match the one given.
+    const badVersions = versions.filter((v) => v.postId !== postId);
+    badVersions.map((v) => badPostIds.add(v.postId));
 
-
-        if (badPostIds.size > 0) {
-            // This slug is unavailable (already/previously in use)
-            const msg = `Slug "${slug}" is unavailable, used on post(s) ${Array.from(badPostIds).toString()}`;
-            console.error(msg);
-            return msg;
-        } else {
-            return false;
-        }
+    if (badPostIds.size > 0) {
+      // This slug is unavailable (already/previously in use)
+      const msg = `Slug "${slug}" is unavailable, used on post(s) ${Array.from(badPostIds).toString()}`;
+      console.error(msg);
+      return msg;
+    } else {
+      return false;
     }
-})
+  },
+});
 
 export const search = query({
-    args: { searchTerm: v.string() },
-    handler: async (ctx, args) => {
-        const authed = await ctx.auth.getUserIdentity()
+  args: { searchTerm: v.string() },
+  handler: async (ctx, args) => {
+    const authed = await ctx.auth.getUserIdentity();
 
-        const results = await ctx.db.query('posts')
-            .withSearchIndex('search_all', q => {
-                const search = q
-                    .search("search", args.searchTerm)
+    const results = await ctx.db
+      .query("posts")
+      .withSearchIndex("search_all", (q) => {
+        const search = q.search("search", args.searchTerm);
 
-                // Search public + draft posts if user is authed;
-                // otherwise, add filter to only search public posts
-                return authed ? search : search.eq('published', true)
-            })
-            .collect();
+        // Search public + draft posts if user is authed;
+        // otherwise, add filter to only search public posts
+        return authed ? search : search.eq("published", true);
+      })
+      .collect();
 
-        return Promise.all(
-            results.map(async (post) => {
-                // Add the author's details to each post.
-                const author = (await ctx.db.get(post.authorId))!;
-                return { ...post, author };
-            }),
-        )
-    }
-})
+    return Promise.all(
+      results.map(async (post) => {
+        // Add the author's details to each post.
+        const author = (await ctx.db.get(post.authorId))!;
+        return { ...post, author };
+      }),
+    );
+  },
+});
--- convex/users.ts
+++ convex/users.ts
@@ -4,68 +4,67 @@ import { auth } from "./auth";
 import schema from "./schema";
 import { crud } from "convex-helpers/server/crud";
 
-export const {
-  create,
-  read,
-  update,
-  destroy
-} = crud(schema, 'users', query, mutation);
-
+export const { create, read, update, destroy } = crud(
+  schema,
+  "users",
+  query,
+  mutation,
+);
 
 export const viewer = query({
   args: {},
   handler: async (ctx) => {
     const userId = await auth.getUserId(ctx);
     return userId ? await ctx.db.get(userId) : null;
-  }
+  },
 });
 
-
-
 export const authoredPosts = query({
-  args: { userId: v.id('users') },
+  args: { userId: v.id("users") },
   handler: async (ctx, args) => {
-    return await ctx.db.query('posts')
-      .withIndex('by_authorId', q => q.eq('authorId', args.userId))
-      .collect()
-  }
-})
+    return await ctx.db
+      .query("posts")
+      .withIndex("by_authorId", (q) => q.eq("authorId", args.userId))
+      .collect();
+  },
+});
 
 export const listAuthors = query({
   args: {},
   handler: async (ctx) => {
-    const users = await ctx.db.query('users').collect();
-    const withPosts = await Promise.all(users.map(async u => {
-      const posts = await authoredPosts(ctx, { userId: u._id });
-      return posts.length ? { ...u, posts } : null
-    }))
-    return withPosts.filter(u => !!u);
-  }
+    const users = await ctx.db.query("users").collect();
+    const withPosts = await Promise.all(
+      users.map(async (u) => {
+        const posts = await authoredPosts(ctx, { userId: u._id });
+        return posts.length ? { ...u, posts } : null;
+      }),
+    );
+    return withPosts.filter((u) => !!u);
+  },
 });
 
 export const byEmail = query({
   args: {
-    email: v.string()
+    email: v.string(),
   },
   handler: async (ctx, args) => {
-    return await ctx.db.query('users')
-      .filter(q => q.eq(q.field('email'), args.email))
+    return await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("email"), args.email))
       .unique();
-  }
-})
+  },
+});
 
 export const getOrSetSlug = mutation({
-  args: { id: v.id('users') },
+  args: { id: v.id("users") },
   handler: async (ctx, args) => {
-    const user = await read(ctx, { id: args.id })
+    const user = await read(ctx, { id: args.id });
     if (user === null) return null;
     if (user.slug) return user.slug;
 
     let slug;
     if (user.name) {
-      slug = encodeURIComponent(user.name
-        .toLowerCase()
-        .replaceAll(" ", "-"));
+      slug = encodeURIComponent(user.name.toLowerCase().replaceAll(" ", "-"));
     } else if (user.email) {
       slug = encodeURIComponent(user.email);
     } else {
@@ -73,15 +72,15 @@ export const getOrSetSlug = mutation({
     }
     await update(ctx, { id: user._id, patch: { slug } });
     return slug;
-  }
-
-})
+  },
+});
 
 export const bySlug = query({
   args: { slug: v.string() },
   handler: async (ctx, args) => {
-    return await ctx.db.query('users')
-      .filter(q => q.eq(q.field('slug'), args.slug))
+    return await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("slug"), args.slug))
       .unique();
-  }
-})
+  },
+});
